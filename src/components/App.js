e513b6682aacb8921e6ea1693f516dd0b9b9f7e2
github username <github email address>
2017-10-03 15:54:50 -0400

    more nav changes

--- src/components/App.js
+++ src/components/App.js
@@ -83,7 +83,7 @@ class App extends Component {
       	<Route path="/" component={Nav}/>
 
       	<Route path="/login" render={(props) => <AuthLoginForm onLogin={this.login} {...props} />}/>
-      	<Route path="/doctors" render={(props) => <AuthDoctorsContainer {...props} />} />
+      	<Route path="/home" render={(props) => <AuthDoctorsContainer {...props} />} />
         <Route path="/myProfile" render={(props) => <AuthMyProfile {...props} user={this.state.user} />} />
 
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react'
+import { logoutUser } from '../services/user';
 
 
 class Nav extends React.Component {
   render() {
     return (
-      <div className="ui header menu">
-        <NavLink activeClassName="active" className="item" to="/doctors">Home</NavLink>
-        <NavLink activeClassName="active" className="item" to="/myProfile">My Profile</NavLink>
-       
+      <div className="ui blue header menu">
+      	
+	        <NavLink activeClassName="active" className="item" to="/home">Home</NavLink>
+	        <NavLink activeClassName="active" className="item" to="/myProfile">My Profile</NavLink>
+	        <NavLink activeClassName="active" className="item right" to="/login">Log In</NavLink>
+       		
       </div>
     )
   }
