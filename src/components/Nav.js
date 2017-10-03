c93b514a6acea42fde220f0a1f041985673d1b22
github username <github email address>
2017-10-03 13:22:27 -0400

    some button change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 class Nav extends React.Component {
   render() {
     return (
       <div className="ui secondary menu">
-        <NavLink activeClassName="active" className="item" to="/doctors">Doctors</NavLink>
+        <NavLink activeClassName="active" className="item" to="/doctors">Home</NavLink>
         <NavLink activeClassName="active" className="item" to="/myProfile">My Profile</NavLink>
        
       </div>
